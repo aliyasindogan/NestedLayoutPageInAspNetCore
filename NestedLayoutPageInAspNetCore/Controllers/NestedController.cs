391700c41e82125010bc8ca18af3fd8eea1e0486
Ali Yasin Doğan <a.yasindogan@gmail.com>
2022-01-01 19:37:40 +0300

    Add project files.

---
+++ NestedLayoutPageInAspNetCore/Controllers/NestedController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestedLayoutPageInAspNetCore.Controllers
+{
+    public class NestedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Nested1()
+        {
+            return View();
+        }
+        public IActionResult Nested2()
+        {
+            return View();
+        }
+
+        public IActionResult Nested3()
+        {
+            return View();
+        }
+    }
+}
---
+++ NestedLayoutPageInAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NestedLayoutPageInAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
